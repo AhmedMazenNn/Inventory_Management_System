b5ac56bc71caf32b09afc3a5363178d0e22c844c
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-12 04:56:59 +0200

    added in accounts app views.py which has classes like login logout register employee which can only be done by manager and the urls.py for the templates added in templates/accounts

--- Inventory_Management_System/accounts/views.py
+++ Inventory_Management_System/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import EmployeeRegistrationForm
 
-# Create your views here.
+User = get_user_model()
+
+class LoginView(AuthLoginView):
+    template_name = "accounts/login.html"
+
+class LogoutView(AuthLogoutView):
+    next_page = "login"
+
+class DashboardView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, "accounts/dashboard.html")
+
+class RegisterEmployeeView(LoginRequiredMixin, UserPassesTestMixin, View):
+    template_name = "accounts/register_employee.html"
+
+    def test_func(self):
+        return self.request.user.role == "manager"
+
+    def get(self, request):
+        form = EmployeeRegistrationForm()
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request):
+        form = EmployeeRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.role = "employee"
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return redirect("dashboard")
+        return render(request, self.template_name, {"form": form})
