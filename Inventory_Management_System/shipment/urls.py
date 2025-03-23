867385bb27ac9b1053b5a21d7bbc6b2ca7b23486
mohamed sobhi <mohamed.sobhi.anwar@gmail.com.com>
2025-03-23 17:37:32 +0200

    enhance access permissions

--- Inventory_Management_System/shipment/urls.py
+++ Inventory_Management_System/shipment/urls.py
@@ -14,18 +14,3 @@ urlpatterns = [
     path('shipment/deliver/<int:pk>/', ShipmentDeliverView.as_view(), name='deliver_shipment'),
     path("add-item/<int:shipment_id>/", ShipmentItemCreateView.as_view(), name="add_shipment_item"),
 ]
-
-
-
-
-
-
-# urlpatterns = [
-#     path('shipment/',list_shipment, name='shipment_list'),
-#     path('shipment/<int:pk>',shipment_detail, name='shipment_detail'),
-#     path('shipment/create',create_shipment, name='create_shipment'),
-#     path('shipment/update/<int:pk>',update_shipment, name='update_shipment'),
-# ]
-# app_name = 'shipment'
-
-
--- Inventory_Management_System/shipment/views.py
+++ Inventory_Management_System/shipment/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.mixins import UserPassesTestMixin,LoginRequiredMixin
 from django.views.generic import CreateView, UpdateView, ListView, DeleteView, FormView, DetailView
 from django.urls import reverse_lazy
-from django.db import transaction
 from django.contrib import messages as message
 from .models import Shipment, ShipmentItem
 from .forms import ShipmentForm, ShipmentItemForm
-from inventory.models import Product
 
-class ShipmentCreateView(CreateView):
+class ShipmentCreateView(LoginRequiredMixin,CreateView):
     model = Shipment
     form_class = ShipmentForm
     template_name = 'shipments/create_shipment.html'
@@ -25,22 +23,31 @@ class ShipmentCreateView(CreateView):
         message.success(self.request, "Shipment created successfully.")
         return super().form_valid(form)
     
-class ShipmentListView(ListView):
+class ShipmentListView(LoginRequiredMixin,ListView):
     model = Shipment
     template_name = 'shipments/shipment_list.html'
     context_object_name = 'shipments'
 
-class ShipmentDeleteView(DeleteView):
+class ShipmentDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Shipment
     template_name = "shipments/confirm.html"
     success_url = reverse_lazy("shipments:shipment_list")
+
+    def test_func(self):
+        return self.request.user.role == 'manager'
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            message.error(self.request,"Only managers can delete shipments.")
+            return redirect("shipments:shipment_list")
+        return super().handle_no_permission()
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["shipment_name"] = self.object.factory_name if self.object else ''
         return context
 
-class ShipmentItemCreateView(FormView):
+class ShipmentItemCreateView(LoginRequiredMixin,FormView):
     template_name = 'shipments/create_shipment.html'  
     form_class = ShipmentItemForm
 
@@ -49,7 +56,6 @@ class ShipmentItemCreateView(FormView):
         self.shipment = get_object_or_404(Shipment, id=shipment_id)
         if self.shipment.status in ["Delivered", "Approved"]:
             message.error(request, "You cannot add items to a delivered or approved shipment.")
-            # return redirect("shipments:shipment_list")
             return redirect("shipments:shipment_detail",pk=self.shipment.id)
         return super().dispatch(request, *args, **kwargs)
     
@@ -76,7 +82,7 @@ class ShipmentItemCreateView(FormView):
     
     success_url = reverse_lazy("shipments:shipment_list")
 
-class ShipmentDetailView(DetailView):
+class ShipmentDetailView(LoginRequiredMixin,DetailView): 
     model = Shipment
     template_name = 'shipments/shipment_detail.html'
     context_object_name = 'shipment'
@@ -86,7 +92,7 @@ class ShipmentDetailView(DetailView):
         context['shipment_items'] = self.object.shipment_items.all()
         return context
 
-class ShipmentUpdateView(UserPassesTestMixin,UpdateView):
+class ShipmentUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = ShipmentItem
     form_class = ShipmentItemForm
     template_name = 'shipments/create_shipment.html'
@@ -97,7 +103,7 @@ class ShipmentUpdateView(UserPassesTestMixin,UpdateView):
     def handle_no_permission(self):
         if self.request.user.is_authenticated:
             message.error(self.request,"Only managers can update shipments.")
-            return redirect("home")
+            return redirect("shipments:shipment_list")
         return super().handle_no_permission()
 
     def dispatch(self, request, *args, **kwargs):
@@ -117,7 +123,7 @@ class ShipmentUpdateView(UserPassesTestMixin,UpdateView):
     def get_success_url(self):
         return reverse_lazy('shipments:shipment_detail', kwargs={'pk': self.object.shipment.id})
 
-class ShipmentApproveView(UserPassesTestMixin,UpdateView):
+class ShipmentApproveView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): 
     model = Shipment
     fields = []
     template_name = 'shipments/shipment_detail.html'
@@ -145,7 +151,7 @@ class ShipmentApproveView(UserPassesTestMixin,UpdateView):
         return redirect('shipments:shipment_list')
 
 
-class ShipmentDeliverView(UserPassesTestMixin,UpdateView):
+class ShipmentDeliverView(LoginRequiredMixin,UserPassesTestMixin,UpdateView): 
     model = Shipment
     fields = []
     template_name = 'shipments/shipment_detail.html'
@@ -169,157 +175,3 @@ class ShipmentDeliverView(UserPassesTestMixin,UpdateView):
         else:
             message.error(request, "You cannot Delivered this shipment.")
         return redirect('shipments:shipment_list')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from django.http import HttpResponseForbidden
-# from django.shortcuts import get_object_or_404, render
-# from .models import Shipment, ShipmentItem
-# from .forms import ShipmentForm,ShipmentItemFormSet
-# from django.shortcuts import redirect
-# from django.contrib.auth.decorators import login_required
-# from django.contrib import messages
-
-
-# @login_required
-# def list_shipment(request):
-#     shipments = Shipment.objects.all()
-#     return render(request, 'shipment/list_of_shipment.html', {
-#         'shipments': shipments
-#     })
-
-# @login_required
-# def shipment_detail(request, pk):
-#     shipment = get_object_or_404(Shipment, pk=pk)
-#     shipment_items = ShipmentItem.objects.filter(shipment=shipment)
-#     return render(request, 'shipment/shipment_details.html', {
-#         'shipment': shipment,
-#         'shipment_items': shipment_items
-#     })
-
-# @login_required
-# def create_shipment(request):
-#     if request.method == 'POST':
-#         shipment_form = ShipmentForm(request.POST)
-#         formset = ShipmentItemFormSet(request.POST)
-#         if shipment_form.is_valid() and formset.is_valid():
-#             shipment = shipment_form.save(commit=False)
-#             shipment.created_by = request.user
-#             if request.user.role == "manager" and shipment.status == "Approved":
-#                 shipment.approved_by = request.user
-#             else:
-#                 shipment.status = "Pending"
-#                 shipment.approved_by = None
-#             shipment.save()
-#             shipment_items = formset.save(commit=False)
-#             for item in shipment_items:
-#                 item.shipment = shipment
-#                 item.save()
-#             messages.success(request, "Shipment created successfully.")
-#             return redirect('shipment:shipment_list')
-#     else:
-#         shipment_form = ShipmentForm()
-#         formset = ShipmentItemFormSet()
-#     return render(request, 'shipment/create_shipment.html', {
-#         'shipment_form': shipment_form,
-#         'formset': formset
-#     })
-
-
-# @login_required
-# def update_shipment(request, pk):
-#     if request.user.role != "manager":
-#         messages.warning(request, "You don't have permission to update.")
-#         return redirect("shipment:shipment_list")
-#     shipment = get_object_or_404(Shipment, pk=pk)
-#     if shipment.status != "Delivered":
-#         if request.method == 'POST':
-#             shipment_form = ShipmentForm(request.POST, instance=shipment)
-#             formset = ShipmentItemFormSet(request.POST, instance=shipment)
-#             if shipment_form.is_valid() and formset.is_valid():
-#                 shipment = shipment_form.save(commit=False)
-#                 shipment.created_by = request.user
-#                 if request.user.role == "manager" and shipment.status == "Approved":
-#                     shipment.approved_by = request.user
-#                 shipment.save()
-#                 shipment_items = formset.save(commit=False)
-#                 for item in shipment_items:
-#                     item.shipment = shipment
-#                     item.save()
-#                 messages.success(request, "Shipment updated successfully.")  
-#                 return redirect("shipment:shipment_list")
-#         else:
-#             shipment_form = ShipmentForm(instance=shipment)
-#             formset = ShipmentItemFormSet(instance=shipment)
-#     else:
-#         messages.warning(request, "You can't update a deliverd shipment.")
-#         return redirect("shipment:shipment_list")
-#     return render(request, 'shipment/update_shipment.html', {
-#         'shipment_form': shipment_form,
-#         'formset': formset
-#     })
