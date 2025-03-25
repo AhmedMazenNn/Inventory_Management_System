982c17df723f90ea27c2495264ee9aa40d76b980
mohamed sobhi <mohamed.sobhi.anwar@gmail.com.com>
2025-03-25 08:38:00 +0200

    Removed migrations from Git

--- Inventory_Management_System/Inventory_Management_System/settings.py
+++ Inventory_Management_System/Inventory_Management_System/settings.py
@@ -87,6 +87,17 @@ DATABASES = {
     'default': dj_database_url.config(default='postgresql://postgres:NJBiEuWsByfULdXTHdiUZExKrnDhmZed@switchyard.proxy.rlwy.net:29360/railway')
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'inventory',  
+#         'USER': 'postgres',         
+#         'PASSWORD': '12345678', 
+#         'HOST': 'localhost',           
+#         'PORT': '5432',                 
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
